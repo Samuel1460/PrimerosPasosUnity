72dac24227105796b448d22692da4bf74195cfc8
Samuel1460 <suruena264@soyudemedellin.edu.co>
2026-08-04 11:32:29 -0500

    Variables

---
+++ Mi unity 2/Assets/_main/Scripts/Player_movement.cs
@@ -0,0 +1,44 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float altura = 1.82f;
+    public int edad = 30;
+    public string nombre = "Roberto";
+    public bool puedeVotar = true;
+
+    public GameObject gameObject;
+    public Rigidbody2D rigidbody2D;
+    public Collider2D collider2D;
+    public SpriteRenderer spriteRenderer;
+    public Transform Transform;
+
+   
+
+
+    private void Start()
+    {
+        Debug.Log(" Hola : " + nombre + " tu edad es : " + edad + " tu altura es: " + altura);
+        rigidbody2D.simulated = false; 
+        spriteRenderer.color = Color.red;
+        transform.position = new Vector3(10f, 0f, 0f );
+
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+
+
+
+
+
+
+
+
+}
